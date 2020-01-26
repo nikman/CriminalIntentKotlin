43c10b129e40c75509e6579450039eb82475f5dc
niku.andrew@gmail.com <niku.andrew@gmail.com>
2020-01-26 13:08:49 +0300

    1st

---
+++ app/src/main/java/ru/niku/criminalintent/CrimeFragment.kt
@@ -0,0 +1,30 @@
+package ru.niku.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        return view
+    }
+}
