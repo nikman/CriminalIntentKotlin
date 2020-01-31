fad9041653876d16375d5fb617f9797afc4b07a2
niku.andrew@gmail.com <niku.andrew@gmail.com>
2020-02-01 00:22:41 +0300

    Constraint layout

--- app/src/main/java/ru/niku/criminalintent/Crime.kt
+++ app/src/main/java/ru/niku/criminalintent/Crime.kt
@@ -6,5 +6,6 @@ import java.util.UUID
 data class Crime(val id: UUID = UUID.randomUUID(),
                  var title: String = "",
                  var date: Date = Date(),
-                 var isSolved: Boolean = false)
+                 var isSolved: Boolean = false,
+                 var requiresPolice: Int = 0)
 
--- app/src/main/java/ru/niku/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/ru/niku/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,7 +17,7 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
-    private lateinit var adapter: CrimeAdapter
+    private var adapter: CrimeAdapter? = null
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -50,10 +51,30 @@ class CrimeListFragment : Fragment() {
         }
     }
 
-    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private inner class CrimeHolder(view: View)
+            : RecyclerView.ViewHolder(view), View.OnClickListener {
 
-        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
-        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private lateinit var crime: Crime
+        
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
+        override fun onClick(v: View?) {
+
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_LONG).show()
+            //crimeRecyclerView.adapter?.notifyItemMoved(0, 5)
+
+        }
 
     }
 
@@ -61,7 +82,17 @@ class CrimeListFragment : Fragment() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
 
-            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            val crimeViewType = getItemViewType(viewType)
+            var resId: Int
+
+            if (crimeViewType == 0) {
+                resId = R.layout.list_item_crime
+            } else {
+                resId = R.layout.list_item_crime_police
+            }
+
+            val view = layoutInflater.inflate(resId, parent, false)
+
             return CrimeHolder(view)
 
         }
@@ -70,10 +101,16 @@ class CrimeListFragment : Fragment() {
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+            /*holder.apply {
+                /*titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()*/
+            }*/
+            holder.bind(crime)
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            val crime = crimes[position]
+            return crime.requiresPolice
         }
     }
 
